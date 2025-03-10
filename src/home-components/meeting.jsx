16fba4b998abbb6b2999c6e71050a95c32f63b8e
prince-morage <princeenajemo@gmail.com>
2025-03-10 14:00:18 +0100

    ty

--- src/home-components/meeting.jsx
+++ src/home-components/meeting.jsx
@@ -6,8 +6,8 @@ import meetingPhoto from "../assets/images/pexels-photo-3194519.jpeg"
 function Meeting(){
  return(
     <>
-    <div className="flex flex-col  md:flex-row w-screen mt-30 pb-30 justify-center bg-neutral-950 p-5" >
-        <section className="w-[98%] md:w-[40%] flex flex-col justify-between rounded-4xl bg-black p-10    h-150 "> 
+    <div className="flex flex-col gap-10 md:gap-0 md:flex-row w-screen mt-30 pb-30 justify-center bg-neutral-950 p-5" >
+        <section className="w-[98%] md:w-[40%] flex flex-col justify-between rounded-4xl bg-black p-10    h-130 md:h-150"> 
             <div className="flex flex-col gap-5">
                 <p className="md:text-2xl primary-color">Need a Project?</p>
                 <h1 className="text-2xl md:text-5xl">
@@ -18,22 +18,22 @@ function Meeting(){
             <hr className="primary-color"/>
 
             <div className="flex w-100 gap-3">
-            <span className="w-12 h-12 blue-bg flex justify-center items-center rounded-full"><img className="w-8 h-8    " src={phoneImg} alt="" /></span>
-                  <span className=" flex flex-col gap-2">  <p className="text-2xl">Phone Number </p>
-                  <h3 className="text-2xl primary-color">+2348066498004</h3>
+            <span className="w-12 h-12 blue-bg flex justify-center items-center rounded-full"><img className="w-5 h-5 md:w-8 md:h-8    " src={phoneImg} alt="" /></span>
+                  <span className=" flex flex-col gap-2">  <p className="md:text-2xl">Phone Number </p>
+                  <h3 className="md:text-2xl primary-color">+2348066498004</h3>
                   </span>
             </div>
 
            <hr className="primary-color"/>   
 
             <div className="flex w-100 gap-3">
-            <span className="w-12 h-12 blue-bg flex justify-center items-center rounded-full"><img className="w-8 h-8    " src={emailImg} alt="" /></span>
-                  <span className=" flex flex-col gap-2"> <p className="text-2xl"> Email </p>
-                  <h3  className="text-2xl primary-color">princeenajemo@gmail.com</h3>
+            <span className="w-12 h-12 blue-bg flex justify-center items-center rounded-full"><img className="w-5 h-5 md:w-8 md:h-8    " src={emailImg} alt="" /></span>
+                  <span className=" flex flex-col gap-2"> <p className="md:text-2xl"> Email </p>
+                  <h3 className="md:text-2xl primary-color">princeenajemo@gmail.com</h3>
                   </span>
             </div>
         </section>
-        <section className="w-[45%] flex justify-center">
+        <section className="w-[98%] md:w-[45%] flex justify-center">
             <img className="h-150 rounded-4xl" src={meetingPhoto} alt="" />
         </section>
     </div>
