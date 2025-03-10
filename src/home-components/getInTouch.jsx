4a779e9156f5a379098bdf6f920a04e25a382bfc
prince-morage <princeenajemo@gmail.com>
2025-03-10 14:27:41 +0100

    ui

--- src/home-components/getInTouch.jsx
+++ src/home-components/getInTouch.jsx
@@ -5,24 +5,24 @@ const GetInTouch = () => {
   return (
     <div>
         <div className='flex flex-col md:flex-row justify-center gap-20' id='contact'>
-            <section className='w-[450px] flex flex-col gap-15'>
-                <h1 className='text-5xl'>Get in Touch</h1>
+            <section className='w-90% md:w-[450px] flex flex-col items-center gap-15'>
+                <h1 className='text-3xl  md:text-5xl'>Get in Touch</h1>
 
                 <form action="" className='flex flex-col items-center gap-13'> 
                     <div className='flex flex-col gap-4 '>
                         <label For="name"> Name</label>
-                        <input type="text" name='name' placeholder='Enter Your Name' required className='focus:outline-none border-none w-[80%] md:w-100' />
-                        <hr className='primary-color w-[80%] md:w-100' />
+                        <input type="text" name='name' placeholder='Enter Your Name' required className='focus:outline-none border-none w-[100%] md:w-100' />
+                        <hr className='primary-color w-[100%] md:w-100' />
                     </div>
                     
                     <div className='flex flex-col gap-4   '>
                         <label For="email"> Email</label>
-                        <input type="email" name='email' placeholder='Enter a valid email address' required  className='focus:outline-none border-none w-[80%] md:w-100'/>
-                        <hr className='primary-color w-[80%] md:w-100  ' />
+                        <input type="email" name='email' placeholder='Enter a valid email address' required  className='focus:outline-none border-none w-[100%] md:w-100'/>
+                        <hr className='primary-color w-[100%] md:w-100  ' />
 
                     </div>
                     
-                    <button className='w-100 h-13 blue-bg rounded-4xl'>SUBMIT</button>
+                    <button className='w-80 h-13 blue-bg rounded-4xl'>SUBMIT</button>
                 </form>
             </section>
             <section>
